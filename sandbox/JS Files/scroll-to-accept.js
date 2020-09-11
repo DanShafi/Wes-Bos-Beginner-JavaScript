850e23d0d6d77ed12fc6d0b8dea991ba15adf121
Danish Shafi <danish.shafi@live.co.uk>
2020-09-11 11:59:44 +0100

    Using intersection observer

--- sandbox/JS Files/scroll-to-accept.js
+++ sandbox/JS Files/scroll-to-accept.js
@@ -1,12 +1,16 @@
-function scrollToAccept() {
-  const terms = document.querySelector('.terms-and-conditions');
-  if (!terms) {
-    // There isn't that item on the page.
+const terms = document.querySelector('.terms-and-conditions');
+const button = document.querySelector('.accept');
+
+function obCallBack(payload) {
+  if (payload[0].intersectionRatio === 1) {
+    button.disabled = false;
+    console.log('REMOVING DISABLED');
   }
-  terms.addEventListener('scroll', e => {
-    console.log(e.currentTarget.scrollTop);
-    console.log(e.currentTarget.scrollHeight);
-  });
 }
 
-scrollToAccept();
+const ob = new IntersectionObserver(obCallBack, {
+  root: terms,
+  threshold: 1,
+});
+
+ob.observe(terms.lastElementChild);
