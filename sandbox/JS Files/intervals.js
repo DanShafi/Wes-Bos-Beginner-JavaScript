1ba685d906fb6f60e29177d78ece81a7e6ef55eb
Danish Shafi <danish.shafi@live.co.uk>
2020-09-20 02:11:44 +0100

    Setting intervals and timeouts

---
+++ sandbox/JS Files/intervals.js
@@ -0,0 +1,42 @@
+console.log('This is the Intervals page');
+
+function pageLoader() {
+  console.log('Your page has loaded.');
+}
+
+// setTimeout(pageLoader, 1000);
+
+// const setImmediateInterval = (funcToRun, ms) => {
+//   funcToRun();
+//   return setInterval(funcToRun, ms);
+// };
+
+// setImmediateInterval(pageLoader, 1000);
+
+function startCountdown(seconds) {
+  let counter = seconds;
+
+  const interval = setInterval(() => {
+    console.log(counter);
+    document.body.innerHTML = `<p>Quick! Click to stop the page from self destructing. You have ${counter} seconds.</p>`;
+    counter--;
+
+    if (counter < 0) {
+      clearInterval(interval);
+    }
+  }, 1000);
+  return counter;
+}
+
+startCountdown(5);
+
+const destroy = () => {
+  document.body.innerHTML = `<p>Mission failed: the page has been destroyed.`;
+};
+
+const destroyTimer = setTimeout(destroy, 5000);
+
+window.addEventListener('click', function() {
+  document.body.innerHTML = '<p>Well done, you have saved the day.</p>';
+  clearTimeout(destroyTimer, startCountdown);
+});
