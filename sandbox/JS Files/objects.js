e4b90dd9263308a991f1a14f2d54ed85d7fb3790
Danish Shafi <danish.shafi@live.co.uk>
2020-09-21 15:00:17 +0100

    Using Lodash modules to copy objects

--- sandbox/JS Files/objects.js
+++ sandbox/JS Files/objects.js
@@ -18,3 +18,28 @@ console.log(aboutMe.languages[0]);
 
 // const propertyToCheck = prompt('What property do you want to check?');
 // console.log(aboutMe[propertyToCheck]);
+
+let name1 = 'Dan';
+const name2 = 'Dan';
+
+console.log(name1 === name2);
+
+name1 = 'Bob';
+name1 = name2;
+
+const person1 = {
+  name: 'Dan',
+  last: 'Shafi',
+};
+
+const person2 = {
+  name: 'Dan',
+  last: 'Shafi',
+};
+
+// Objects are referenced by object name, not values.
+
+// const person3 = { ...person1 };
+const person3 = _.cloneDeep(person1);
+person3.name = 'Bob';
+person3.clothing = 100;
